0aa72c2c55285d12fa0d56e6c832cb54770808a3
Santiago Lapresta <santiago.lapresta@gmail.com>
2014-10-25 13:38:36 +0200

    Move tests to the end of the file and rearrange methods

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,96 @@ fn merge_env_hashes(
 	result
 }
 
+fn parse_env(data: Vec<u8>) -> HashMap<Vec<u8>, Vec<u8>> {
+	let line_splitter = regex!(r"[\r\n]+");
+	let env_format = regex!(
+		r"^(?P<key>[A-Za-z_][A-Za-z0-9_]*)\s*=\s*(?P<value>.+)$"
+	);
+
+	let data_string = String::from_utf8_lossy(data.as_slice());
+	let data_slice = data_string.as_slice();
+
+	line_splitter.split(data_slice).filter_map(|line| {
+		env_format.captures(line).and_then(|captures| {
+			let key = captures.name("key");
+			let value = captures.name("value");
+
+			// regex captures return empty string on failure
+			if key == "" || value == "" {
+				None
+			} else {
+				Some((
+					String::from_str(key).into_bytes(),
+					String::from_str(value).into_bytes()
+				))
+			}
+		})
+	}).collect()
+}
+
+impl Dotenv {
+	pub fn from_str(s: &str) -> Dotenv {
+		Dotenv::from_bytes(String::from_str(s).into_bytes())
+	}
+	pub fn from_bytes(bytes: Vec<u8>) -> Dotenv {
+		let dotenv_hash = parse_env(bytes);
+		let env_hash = os::env_as_bytes().into_iter().collect();
+		Dotenv{env: merge_env_hashes(env_hash, dotenv_hash)}
+	}
+
+	pub fn from_file(mut file: File) -> IoResult<Dotenv> {
+		file.read_to_end().map(|file_contents|
+			Dotenv::from_bytes(file_contents)
+		)
+	}
+
+	pub fn from_path(path: &Path) -> IoResult<Dotenv> {
+		File::open(path).and_then(|file| {
+			Dotenv::from_file(file)
+		})
+	}
+
+	pub fn getenv(&self, n: &str) -> Option<String> {
+		let n_bytes = &String::from_str(n).into_bytes();
+		self.env.find(n_bytes).and_then(|bytes| {
+			String::from_utf8(bytes.clone()).ok()
+		})
+	}
+
+	pub fn getenv_as_bytes(&self, n: &str) -> Option<Vec<u8>> {
+		let n_bytes = &String::from_str(n).into_bytes();
+		self.env.find(n_bytes).map(|bytes| {
+			bytes.clone()})
+		}
+
+	pub fn env(&self) -> Vec<(String, String)> {
+		self.env.iter().filter_map(|(key, value)| {
+			String::from_utf8(key.clone()).ok().and_then(|key_string| {
+				String::from_utf8(value.clone()).ok().map(|value_string| {
+					(key_string.clone(), value_string)
+				})
+			})
+		}).collect()
+	}
+
+	pub fn env_as_bytes(&self) -> Vec<(Vec<u8>, Vec<u8>)> {
+		self.env.iter().map(|(key, value)| {
+			(key.clone(), value.clone())
+		}).collect()
+	}
+}
+
+impl Default for Dotenv {
+	fn default() -> Dotenv {
+		os::self_exe_path().as_mut().and_then(|path| {
+			path.push(".env");
+			Dotenv::from_path(path).ok()
+		}).unwrap_or(
+			Dotenv::from_bytes(Default::default())
+		)
+	}
+}
+
 #[test]
 fn merge_env_hashes_test() {
 	let hash_a: HashMap<Vec<u8>, Vec<u8>> = vec![
@@ -53,33 +143,6 @@ fn merge_env_hashes_test() {
 	let actual_hash = merge_env_hashes(hash_a, hash_b);
 
 	assert_eq!(expected_hash, actual_hash)
-}
-
-fn parse_env(data: Vec<u8>) -> HashMap<Vec<u8>, Vec<u8>> {
-	let line_splitter = regex!(r"[\r\n]+");
-	let env_format = regex!(
-		r"^(?P<key>[A-Za-z_][A-Za-z0-9_]*)\s*=\s*(?P<value>.+)$"
-	);
-
-	let data_string = String::from_utf8_lossy(data.as_slice());
-	let data_slice = data_string.as_slice();
-
-	line_splitter.split(data_slice).filter_map(|line| {
-		env_format.captures(line).and_then(|captures| {
-			let key = captures.name("key");
-			let value = captures.name("value");
-
-			// regex captures return empty string on failure
-			if key == "" || value == "" {
-				None
-			} else {
-				Some((
-					String::from_str(key).into_bytes(),
-					String::from_str(value).into_bytes()
-				))
-			}
-		})
-	}).collect()
 }
 
 #[test]
@@ -132,67 +195,4 @@ fn parse_env_test_edges() {
 	let actual_hash = parse_env(input);
 
 	assert_eq!(expected_hash, actual_hash);
-}
-
-impl Dotenv {
-	pub fn from_str(s: &str) -> Dotenv {
-		Dotenv::from_bytes(String::from_str(s).into_bytes())
-	}
-	pub fn from_bytes(bytes: Vec<u8>) -> Dotenv {
-		let dotenv_hash = parse_env(bytes);
-		let env_hash = os::env_as_bytes().into_iter().collect();
-		Dotenv{env: merge_env_hashes(env_hash, dotenv_hash)}
-	}
-
-	pub fn from_file(mut file: File) -> IoResult<Dotenv> {
-		file.read_to_end().map(|file_contents|
-			Dotenv::from_bytes(file_contents)
-		)
-	}
-
-	pub fn from_path(path: &Path) -> IoResult<Dotenv> {
-		File::open(path).and_then(|file| {
-			Dotenv::from_file(file)
-		})
-	}
-
-	pub fn getenv(&self, n: &str) -> Option<String> {
-		let n_bytes = &String::from_str(n).into_bytes();
-		self.env.find(n_bytes).and_then(|bytes| {
-			String::from_utf8(bytes.clone()).ok()
-		})
-	}
-
-	pub fn getenv_as_bytes(&self, n: &str) -> Option<Vec<u8>> {
-		let n_bytes = &String::from_str(n).into_bytes();
-		self.env.find(n_bytes).map(|bytes| {
-			bytes.clone()})
-		}
-
-	pub fn env_as_bytes(&self) -> Vec<(Vec<u8>, Vec<u8>)> {
-		self.env.iter().map(|(key, value)| {
-			(key.clone(), value.clone())
-		}).collect()
-	}
-
-	pub fn env(&self) -> Vec<(String, String)> {
-		self.env.iter().filter_map(|(key, value)| {
-			String::from_utf8(key.clone()).ok().and_then(|key_string| {
-				String::from_utf8(value.clone()).ok().map(|value_string| {
-					(key_string.clone(), value_string)
-				})
-			})
-		}).collect()
-	}
-}
-
-impl Default for Dotenv {
-	fn default() -> Dotenv {
-		os::self_exe_path().as_mut().and_then(|path| {
-			path.push(".env");
-			Dotenv::from_path(path).ok()
-		}).unwrap_or(
-			Dotenv::from_bytes(Default::default())
-		)
-	}
 }
