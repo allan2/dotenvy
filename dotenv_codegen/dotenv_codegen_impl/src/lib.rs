eaeb9d3500d1d02bfc9b16e4f879324a33ddf03f
Andy Russell <arussell123@gmail.com>
2017-11-15 16:02:50 -0500

    rewrite `dotenv!` to compile on stable
    Removes syntex, and instead uses `proc-macro-hack` to compile on stable.

---
+++ dotenv_codegen/dotenv_codegen_impl/src/lib.rs
@@ -0,0 +1,60 @@
+#[macro_use]
+extern crate proc_macro_hack;
+
+#[macro_use]
+extern crate quote;
+
+extern crate dotenv;
+extern crate syn;
+
+use std::env;
+
+use dotenv::DotenvError::Parsing;
+use dotenv::dotenv;
+use syn::{TokenTree, Token, Lit};
+
+proc_macro_expr_impl! {
+    pub fn expand_dotenv(input: &str) -> String {
+        if let Err(Parsing { line }) = dotenv() {
+            panic!("Error parsing .env file: {}", line);
+        }
+
+        // Either everything was fine, or we didn't find an .env file (which we ignore)
+        expand_env(input)
+    }
+}
+
+fn expand_env(input: &str) -> String {
+    let tts = syn::parse_token_trees(input).unwrap();
+
+    if tts.is_empty() {
+        panic!("dotenv! takes 1 or 2 arguments");
+    }
+
+    let mut tts = tts.into_iter();
+
+    let var = match tts.next().unwrap() {
+        TokenTree::Token(Token::Literal(Lit::Str(s, _))) => s,
+        _ => panic!("expected a string literal as the first argument"),
+    };
+
+    match tts.next() {
+        Some(TokenTree::Token(Token::Comma)) | None => (),
+        _ => panic!("expected a comma-separated list of expressions"),
+    }
+
+    let err_msg = match tts.next() {
+        Some(TokenTree::Token(Token::Literal(Lit::Str(s, _)))) => s,
+        None => format!("environment variable `{}` not defined", var),
+        _ => panic!("expected a string literal as the second argument"),
+    };
+
+    if tts.next().is_some() {
+        panic!("dotenv! takes 1 or 2 arguments");
+    }
+
+    match env::var(&var) {
+        Ok(val) => quote!(#val).to_string(),
+        Err(_) => panic!("{}", err_msg),
+    }
+}
--- dotenv_codegen/src/lib.rs
+++ dotenv_codegen/src/lib.rs
@@ -1,28 +1,12 @@
-#![cfg_attr(not(feature = "with-syntex"), feature(rustc_private))]
-#![deny(warnings)]
+#[macro_use]
+extern crate proc_macro_hack;
 
-extern crate dotenv;
+#[allow(unused_imports)]
+#[macro_use]
+extern crate dotenv_codegen_impl;
+#[doc(hidden)]
+pub use dotenv_codegen_impl::*;
 
-#[cfg(feature = "with-syntex")]
-extern crate syntex;
-
-#[cfg(feature = "with-syntex")]
-extern crate syntex_syntax as syntax;
-
-#[cfg(not(feature = "with-syntex"))]
-extern crate syntax;
-
-#[cfg(not(feature = "with-syntex"))]
-extern crate rustc_plugin;
-
-mod dotenv_macro;
-
-#[cfg(feature = "with-syntex")]
-pub fn register(reg: &mut syntex::Registry) {
-    reg.add_macro("dotenv", dotenv_macro::expand_dotenv);
-}
-
-#[cfg(not(feature = "with-syntex"))]
-pub fn register(reg: &mut rustc_plugin::Registry) {
-    reg.register_macro("dotenv", dotenv_macro::expand_dotenv);
+proc_macro_expr_decl! {
+    dotenv! => expand_dotenv
 }
