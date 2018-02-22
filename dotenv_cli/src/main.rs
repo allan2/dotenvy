8d2ffc011a7cf853c0ff10d21a89eefec17c217f
Adam Sharp <adam@sharplet.me>
2018-02-21 23:21:20 -0500

    Stop parsing trailing args without need for --
    Previously, clap would try and parse arguments from the entire arg list,
    even though we only care about the options we've explicitly specified.
    This means that an invocation like
    
        dotenv -f .env.development sh -c 'echo $DATABASE_URL'
    
    would incorrectly exit with an error, because '-c' is an unrecognised
    argument.
    
    Following the suggested approach in
    https://github.com/kbknapp/clap-rs/issues/484#issuecomment-211126542, we
    use the `AllowExternalSubcommands` setting to treat everything after the
    option list as a subcommand, letting us pass through the remaining
    arguments to the command untouched. The tradeoff is that we need to
    provide a custom error message when no command is supplied.

--- dotenv_cli/src/main.rs
+++ dotenv_cli/src/main.rs
@@ -17,38 +17,45 @@ macro_rules! die {
     });
 }
 
+fn make_command(name: &str, args: Vec<&str>) -> Command {
+    let mut command = Command::new(name);
+
+    for arg in args {
+        command.arg(arg);
+    }
+
+    return command;
+}
+
 fn main() {
     let matches = App::new("dotenv")
         .about("Run a command using the environment in a .env file")
+        .usage("dotenv <COMMAND> [ARGS]...")
+        .setting(AppSettings::AllowExternalSubcommands)
         .setting(AppSettings::ArgRequiredElseHelp)
-        .setting(AppSettings::TrailingVarArg)
         .setting(AppSettings::UnifiedHelpMessage)
         .arg(Arg::with_name("FILE")
              .short("f")
              .long("file")
              .takes_value(true)
              .help("Use a specific .env file (defaults to .env)"))
-        .arg(Arg::with_name("COMMAND")
-             .required(true)
-             .help("The command to run"))
-        .arg(Arg::with_name("ARGS")
-             .multiple(true)
-             .help("Command arguments (optional)"))
         .get_matches();
 
     match matches.value_of("FILE") {
         None => dotenv::dotenv(),
         Some(file) => dotenv::from_filename(file),
-    }.unwrap_or_else(|e| die!("error: Failed to load environment: {}", e));
+    }.unwrap_or_else(|e| die!("error: failed to load environment: {}", e));
 
-    let mut command = Command::new(matches.value_of("COMMAND").unwrap());
-    let args = matches.values_of("ARGS")
-        .map(|v| v.collect())
-        .unwrap_or(Vec::new());
+    let mut command = match matches.subcommand() {
+        (name, Some(matches)) => {
+            let args = matches.values_of("")
+                .map(|v| v.collect())
+                .unwrap_or(Vec::new());
 
-    for argument in args {
-        command.arg(argument);
-    }
+            make_command(name, args)
+        },
+        _ => die!("error: missing required argument <COMMAND>"),
+    };
 
     let error = command.exec();
     die!("fatal: {}", error);
