e7f5e5f7c8250ef57de4dfbdbfd2ab55e30cd036
Daniel Griffen <daniel@griffen.io>
2019-05-07 18:57:29 -0700

    rename codegen_impl crate

--- dotenv_codegen/src/lib.rs
+++ dotenv_codegen/src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate proc_macro_hack;
 
-extern crate dotenv_codegen_impl;
+extern crate dotenv_codegen_implementation;
 
 #[proc_macro_hack]
-pub use dotenv_codegen_impl::dotenv;
+pub use dotenv_codegen_implementation::dotenv;
