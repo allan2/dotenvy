84f661b8a0aec2813b20f71fc89593ce1473e90e
Robin Vobruba <hoijui.quaero@gmail.com>
2022-02-27 18:27:28 +0100

    Extends multi-line integration-test

--- dotenv/tests/test-multiline.rs
+++ dotenv/tests/test-multiline.rs
@@ -9,9 +9,36 @@ use crate::common::*;
 fn test_multiline() {
     let value = "-----BEGIN PRIVATE KEY-----\n-----END PRIVATE KEY-----\\n\\\"QUOTED\\\"";
     let weak = "-----BEGIN PRIVATE KEY-----\n-----END PRIVATE KEY-----\n\"QUOTED\"";
-    let dir = tempdir_with_dotenv(&format!("WEAK=\"{}\"\nSTRONG='{}'", value, value)).unwrap();
+    let dir = tempdir_with_dotenv(&format!(
+        r#"
+KEY=my\ cool\ value
+KEY3="awesome stuff \"mang\"
+more
+on other
+lines"
+KEY4='hello '\''fds'"
+good ' \'morning"
+WEAK="{}"
+STRONG='{}'
+"#,
+        value, value
+    ))
+    .unwrap();
 
     dotenv().ok();
+    assert_eq!(var("KEY").unwrap(), r#"my cool value"#);
+    assert_eq!(
+        var("KEY3").unwrap(),
+        r#"awesome stuff "mang"
+more
+on other
+lines"#
+    );
+    assert_eq!(
+        var("KEY4").unwrap(),
+        r#"hello 'fds
+good ' 'morning"#
+    );
     assert_eq!(var("WEAK").unwrap(), weak);
     assert_eq!(var("STRONG").unwrap(), value);
 
