24890e824abb377c7488b2974ed4072f54aa3f72
Allan Zhang <al@ayz.ai>
2022-03-08 12:42:18 -0330

    Update crate name in macro tests

--- dotenv_codegen/tests/basic_dotenv_macro.rs
+++ dotenv_codegen/tests/basic_dotenv_macro.rs
@@ -1,12 +1,12 @@
 #[test]
 fn dotenv_works() {
-    assert_eq!(dotenvy_codegen::dotenv!("CODEGEN_TEST_VAR1"), "hello!");
+    assert_eq!(dotenvy_macro::dotenv!("CODEGEN_TEST_VAR1"), "hello!");
 }
 
 #[test]
 fn two_argument_form_works() {
     assert_eq!(
-        dotenvy_codegen::dotenv!(
+        dotenvy_macro::dotenv!(
             "CODEGEN_TEST_VAR2",
             "err, you should be running this in the 'dotenv_codegen' \
              directory to pick up the right .env file."
