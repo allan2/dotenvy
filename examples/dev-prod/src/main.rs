8802f72fd572649b14aa39283e6ba9854689d21e
Allan <al@ayz.ai>
2023-03-17 01:26:38 -0400

    Add dev prod example (#55)
    * Add dev-prod example
    
    * A word
    
    * Fix typo in dotenv_override
    
    Co-authored-by: Christopher Morton <sonro@gmx.com>
    
    * Use std::env::var after explicit dotenv loading
    
    Co-authored-by: Christopher Morton <sonro@gmx.com>
    
    * Incorporate all suggestions
    
    - remove FromStr
    - reorder commands
    
    ---------
    
    Co-authored-by: Christopher Morton <sonro@gmx.com>

---
+++ examples/dev-prod/src/main.rs
@@ -0,0 +1,46 @@
+use std::{
+    env,
+    fmt::{self},
+};
+
+#[derive(PartialEq)]
+enum AppEnv {
+    Dev,
+    Prod,
+}
+
+/// A common setup that:
+///  - loads from a .env file in dev mode
+///  - loads from the envrironment in prod mode
+///
+/// A few commands to try:
+/// 1) `cargo run`
+/// 2) `APP_ENV=prod cargo run`
+/// 3) `APP_ENV=prod HOST=prod.com cargo run`
+fn main() {
+    let app_env = match env::var("APP_ENV") {
+        Ok(v) if v == "prod" => AppEnv::Prod,
+        _ => AppEnv::Dev,
+    };
+
+    println!("Running in {app_env} mode");
+
+    if app_env == AppEnv::Dev {
+        match dotenvy::dotenv() {
+            Ok(path) => println!(".env read successfully from {}", path.display()),
+            Err(e) => println!("Could not load .env file: {e}"),
+        };
+    }
+
+    let host = env::var("HOST").expect("HOST not set");
+    println!("Host: {host}");
+}
+
+impl fmt::Display for AppEnv {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AppEnv::Dev => write!(f, "dev"),
+            AppEnv::Prod => write!(f, "prod"),
+        }
+    }
+}
