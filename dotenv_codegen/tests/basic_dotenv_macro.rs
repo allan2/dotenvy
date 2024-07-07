b571a03c9a43c8eb1721801b01e653d270dd707b
Saber Haj Rabiee <saber.hajrabiee@gmail.com>
2024-07-07 03:33:02 -0700

    added test for `dotenv_override!` macro

--- dotenv_codegen/tests/basic_dotenv_macro.rs
+++ dotenv_codegen/tests/basic_dotenv_macro.rs
@@ -3,3 +3,9 @@ fn dotenv_works() {
     dotenvy_macro::dotenv!();
     assert_eq!(env!("CODEGEN_TEST_VAR1"), "hello!");
 }
+
+#[test]
+fn dotenv_override_works() {
+    dotenvy_macro::dotenv_override!();
+    assert_eq!(env!("USER"), "dotenv!");
+}
