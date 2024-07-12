588959d27855252be55450cf5d105b07bd341d2f
Christopher Morton <sonro@gmx.com>
2024-07-12 11:13:04 +0100

    Refactor dotenv usage into wrapper

--- dotenv/tests/integration/util/tests.rs
+++ dotenv/tests/integration/util/tests.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use super::*;
 
 mod default_env {
@@ -20,7 +22,7 @@ mod default_env {
     #[test]
     fn envfile_loaded_vars_state() {
         test_in_default_env(|| {
-            dotenvy::dotenv().expect("Default TestEnv should have .env file");
+            dotenv_wrapper().expect("Default TestEnv should have .env file");
             // dotenv() does not override existing var
             assert_env_var(TEST_EXISTING_KEY, TEST_EXISTING_VALUE);
             assert_env_var(TEST_KEY, TEST_VALUE);
@@ -44,7 +46,7 @@ mod testenv_init {
 
         test_in_env(init_testenv, || {
             assert!(!envfile_path.exists());
-            assert!(dotenvy::dotenv().is_err());
+            assert!(dotenv_wrapper().is_err());
         });
     }
 }
@@ -68,7 +70,7 @@ mod testenv_init_with_envfile {
     fn default_envfile_loaded_vars_state() {
         let init_testenv = TestEnv::init_with_envfile(create_default_envfile());
         test_in_env(init_testenv, || {
-            dotenvy::dotenv().expect("Default TestEnv should have .env file");
+            dotenv_wrapper().expect("Default TestEnv should have .env file");
             // dotenv() does not override existing var
             // but existing key is not set in this testenv
             assert_env_var(TEST_EXISTING_KEY, TEST_OVERRIDING_VALUE);
@@ -82,7 +84,7 @@ fn assert_envfile_exists_in_testenv(testenv: TestEnv) {
 
     test_in_env(testenv, || {
         assert!(envfile_path.exists());
-        assert!(dotenvy::dotenv().is_ok());
+        assert!(dotenv_wrapper().is_ok());
     });
 }
 
@@ -91,4 +93,8 @@ fn assert_default_keys_not_set_in_testenv(testenv: TestEnv) {
         assert_env_var_unset(TEST_EXISTING_KEY);
         assert_env_var_unset(TEST_KEY);
     });
+}
+
+fn dotenv_wrapper() -> dotenvy::Result<PathBuf> {
+    dotenvy::dotenv()
 }
