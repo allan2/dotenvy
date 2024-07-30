b764212eaa1122c586d393e30380186676970f24
Christopher Morton <sonro@gmx.com>
2024-07-30 17:06:48 +0100

    Fix unnecessary assert messages

--- test_util/src/tests/envfile.rs
+++ test_util/src/tests/envfile.rs
@@ -6,30 +6,21 @@ fn create_default() {
         "{DEFAULT_TEST_KEY}={DEFAULT_TEST_VALUE}\n{DEFAULT_EXISTING_KEY}={DEFAULT_OVERRIDING_VALUE}",
     );
     let actual = create_default_envfile();
-    assert_eq!(
-        expected, actual,
-        "envfile should be created with default values"
-    );
+    assert_eq!(expected, actual);
 }
 
 #[test]
-fn create_invalid() {
+fn create_without_equals() {
     let expected = format!(
         "{DEFAULT_TEST_KEY}{DEFAULT_TEST_VALUE}\n{DEFAULT_EXISTING_KEY}{DEFAULT_OVERRIDING_VALUE}",
     );
     let actual = create_invalid_envfile();
-    assert_eq!(
-        expected, actual,
-        "envfile should be created without equals sign"
-    );
+    assert_eq!(expected, actual);
 }
 
 #[test]
 fn create_custom() {
     let expected = expected_envfile(CUSTOM_VARS);
     let actual = create_custom_envfile(CUSTOM_VARS);
-    assert_eq!(
-        expected, actual,
-        "envfile should be created with custom values"
-    );
+    assert_eq!(expected, actual);
 }
